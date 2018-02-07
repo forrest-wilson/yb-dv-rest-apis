404032eefc8ba1ac9f8765e5aadef605aeaccff2
Forrest Wilson <forrest.wilson@me.com>
2018-02-07 15:12:11 +1300

    Working Google map that takes an input and returns a set of markers on the map

--- app.js
+++ app.js
@@ -1,36 +1,61 @@
-$(document).ready(function() {
-    $("#submit").on("click", function(e) {
-        e.preventDefault();
-        var value = $("#input").val();
-
-        $.ajax({
-            method: "POST",
-            url: "http://maps.googleapis.com/maps/api/geocode/json?address=" + value,
-            dataType: "json",
-            success: function(data) {
-                console.log(data);
-                var out = $("#output");
-                out.empty();
-
-                // Checks for an empty result array
-                if (data.results.length === 0) {
-                    console.log("Empty array");
-                    out.append("<p>Your search returned no results. Please try again.</p>")
-                } else {
-                    for (var i = 0; i < data.results.length; i++) {
-                        var res = data.results[i];
-                        out.append($("<h2>" + res.formatted_address + "</h2>"));
-                        out.append($("<p>Latitude: " + res.geometry.location.lat + "</p>"));
-                        out.append($("<p>Longitude: " + res.geometry.location.lng + "</p>"));
-                    }
-                }
-            },
-            error: function(error) {
-                console.log("Oh no, something went wrong! ", error);
-                var out = $("#output");
-                out.empty();
-                out.append("<p>Please enter a search term.</p>");
-            }
-        });
+// Map Initialization
+var map;
+var markers = [];
+function initMap() {
+    map = new google.maps.Map(document.getElementById('output'), {
+        zoom: 2,
+        maxZoom: 12,
+        center: new google.maps.LatLng(2.8,-187.3),
+        mapTypeId: 'terrain'
     });
+}
+
+// Functions
+function ajax(method, url, success) {
+    $.ajax({
+        method: method,
+        url: url,
+        dataType: "json",
+        success: success,
+        error: function() {
+            console.log("Oh no, something went wrong! ", error);
+        }
+    });
+}
+
+function drawMarkers(data) {
+    for (var i = 0; i < data.results.length; i++) {
+        var coords = data.results[i].geometry.location;
+        var latLng = new google.maps.LatLng(coords.lat,coords.lng);
+        var marker = new google.maps.Marker({
+            position: latLng,
+            map: map
+        });
+        markers.push(marker);
+    }
+    zoomBounds();
+}
+
+function zoomBounds() {
+    var bounds = new google.maps.LatLngBounds();
+    for (var i = 0; i < markers.length; i++) {
+        bounds.extend(markers[i].getPosition());
+    }
+    map.fitBounds(bounds);
+}
+
+function removeMarkers() {
+    for (var i = 0; i < markers.length; i++) {
+        markers[i].setMap(null);
+    }
+    markers = [];
+}
+
+// Event handler
+$("#submit").on("click", function(e) {
+    e.preventDefault();
+    var searchTerm = $("#input").val();
+
+    removeMarkers();
+    ajax("POST", "http://maps.googleapis.com/maps/api/geocode/json?address=" + searchTerm, drawMarkers);
 });
